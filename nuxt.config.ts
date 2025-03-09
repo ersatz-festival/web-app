d0d530f2c30374cb45b46e09af4843b06cd6c2b4
qtipee <quent.vallat@gmail.com>
2025-03-09 17:36:15 +0100

    Add baseURL property for Github Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
     ssr: false,
+    app: {
+        baseURL: '/web-app/',
+    },
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
     css: ['~/assets/css/main.css'],
