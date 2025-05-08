c407444fbd3fc8eeebfed93c162d354e54dd6445
malo-romano <malo.romano@gmail.com>
2025-05-08 21:55:00 +0200

    billets soutiens

--- pages/billets/index.vue
+++ pages/billets/index.vue
@@ -25,6 +25,10 @@
                     </tr>
                 </tbody>
             </table>
+            <p class="mt-4 text-xl md:text-2xl">
+                Nous proposons un prix normal, et un prix de soutien — un peu plus élevé — pour les coeurs généreux qui
+                veulent donner un coup de pouce à notre petit festival (et gagner notre amour éternel) !
+            </p>
         </section>
 
         <section class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
